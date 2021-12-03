303cbcfd6af3f183141937d5bbb2685ba3f31075
Benjamin Maynor <BenjaminMaynor@gmail.com>
2021-12-03 09:57:48 -0600

    Day 3

---
+++ Day3/Program.cs
@@ -0,0 +1,31 @@
+﻿void Part1() {
+
+	int lineCount = 0;
+
+	IEnumerable<string> f = System.IO.File.ReadLines(@"./input.real.txt");
+	int[] sum = new int[f.First().Count()];
+
+	// Sum each column in the input.
+	foreach (string line in f)
+	{
+		lineCount++;
+		for (int i = 0; i < line.Count(); i++) {
+			if (line[i] == '1') {
+				sum[i]++;
+			}
+		}
+	}
+
+	int check = lineCount / 2;
+	int gamma = 0;
+	int epsilon = 0;
+	for (int i = sum.Count()-1; i >= 0; i--) {
+		int magnitude = (int)Math.Pow(2, ((sum.Count()-1) - i));
+		gamma += (sum[i] > check ? 1 : 0) * magnitude;
+		epsilon += (sum[i] < check ? 1 : 0) * magnitude;
+	}
+
+	Console.WriteLine($"Gamma: {gamma} Epsilon: {epsilon} Product: {gamma * epsilon}");
+}
+
+Part1();

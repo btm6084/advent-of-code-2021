930a3883b6959d1c70f10b1767529174da2d471d
Benjamin Maynor <BenjaminMaynor@gmail.com>
2021-12-18 16:20:53 -0600

    Day 16

---
+++ Day16/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace AdventOfCode.Day16 {
+	public static class Day16 {
+
+		public static void Main() {
+			char[] bits = InputParser.Parse("./input.example.txt", x => x).First().ToCharArray();
+			Part1(bits);
+			Part2();
+		}
+
+		public static void Part1(char[] bits) {
+			for(int i = 0; i < bits.Count(); i++) {
+				Console.WriteLine(bits[i].ToString());
+			}
+		}
+
+		public static void Part2() {
+		}
+	}
+}
